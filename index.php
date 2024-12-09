78bf5f4cb28c318a21ea71d47cc2b88052b6e887
Вероника <veronikaosipova2022@gmail.com>
2024-12-09 13:34:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/** @var PDO $pdo */
+$pdo = require $_SERVER['DOCUMENT_ROOT'].'/Products-testing/db.php';
+$products = $pdo->query("SELECT * FROM products");
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Продукция</title>
+</head>
+<body>
+<h1>Список продукции</h1>
+<a href="supplies/"><button>Поставки товара</button></a>
+<a href="create.php"><button>Добавить новый товар</button></a>
+<table>
+    <thead>
+    <tr>
+        <td>id</td>
+        <td>Наименование</td>
+        <td>Цена</td>
+        <td>Артикул</td>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $product): ?>
+    <tr>
+        <td><?= $product['id']?></td>
+        <td><?= $product['name']?></td>
+        <td><?= $product['price']?></td>
+        <td><?= $product['article']?></td>
+        <td><a href="edit.php?id=<?= $product['id']?>">Изменить</a></td>
+        <td><a href="actions/delete.php?id=<?= $product['id']?>">Удалить</a></td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</body>
+</html>
